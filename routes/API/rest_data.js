e942b65da83975d79a40d567356238701b185918
Mario Torres <mario291187@gmail.com>
2020-03-12 17:33:29 -0600

    Backend

--- routes/API/rest_data.js
+++ routes/API/rest_data.js
@@ -1,4 +1,5 @@
-const rp = require('request-promise');
+const axios = require('axios');
+const qs = require('qs');
 
 /*
 const fetchEntities = endpoint => {
@@ -23,47 +24,65 @@ const fetchEntities = endpoint => {
 };
 */
 
-const fetchData = (endpoint, options) => {
-    return getToken(endpoint).then(token_data => {
-        const {access_token} = token_data;
-
-        let opts = {
-            uri: endpoint.url,
-            method: 'POST',
-            qs: {
-                access_token: access_token
-            },
-            body: options,
-            json: true
-        };
-
-        console.log(opts.body);
-        return rp(opts).then( data => {
-            data.supplier_name = endpoint.supplier_name;
-            data.supplier_id = endpoint.supplier_id;
-            data.levels = endpoint.levels;
-            data.endpoint_type = endpoint.type;
-            return data;
-        });
-    });
-};
-
 const getToken = endpoint => {
+
     const opts = {
-        uri: endpoint.token_url,
-        method: 'POST',
-        contentType: 'x-www-form-urlencoded',
-        form: {
+        url: endpoint.token_url,
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': "Basic " + Buffer.from(`${endpoint.client_id}:${endpoint.client_secret}`).toString('base64')
+        },
+        data: qs.stringify({
             grant_type: 'password',
             username: endpoint.username,
             password: endpoint.password,
             client_id: endpoint.client_id,
             client_secret: endpoint.client_secret
-        },
+        }),
         json: true
     };
 
-    return rp(opts);
+    return axios(opts).catch(e => {
+        console.log(e);
+        return {error: true};
+    });
+};
+
+const fetchData = (endpoint, options) => {
+    return getToken(endpoint).then(response => {
+
+        if (typeof response.error !== 'undefined'){
+            return {error: true};
+        }
+
+        const {data} = response;
+        const {access_token} = data;
+
+        let opts = {
+            url: endpoint.url,
+            method: 'post',
+            params: {
+                access_token: access_token
+            },
+            data: options,
+            json: true
+        };
+
+        //console.log(opts);
+
+        return axios(opts).then( request => {
+            let {data} = request;
+            data.supplier_name = endpoint.supplier_name;
+            data.supplier_id = endpoint.supplier_id;
+            data.levels = endpoint.levels;
+            data.endpoint_type = endpoint.type;
+            return data;
+        }).catch(e => {
+            console.log(e);
+            return {error: true};
+        });
+    });
 };
 
 module.exports = {
--- routes/API/v1.js
+++ routes/API/v1.js
@@ -1,23 +1,93 @@
 var express = require('express');
 const cors = require('cors');
 const {fetchData} = require('./rest_data');
+const endpoints = require('../../endpoints');
 
 var router = express.Router();
 router.use(cors());
 
 router.get('/', (req, res) => {
-
-    res.json({});
+    res.json({
+        "title": "API del Sistema 1",
+        "version": '1.0'
+    });
 });
-
 
 router.post('/summary', (req, res) => {
 
-   res.json({});
+    const { body } = req;
+    const { nivel_gobierno, institucion } = body;
+
+    let options = {
+        page: 1,
+        pageSize: 1,
+        query : {}
+    };
+
+    const params = [
+        'nombres',
+        'primerApellido',
+        'segundoApellido'
+    ];
+
+    for (let k of params){
+        if (body.hasOwnProperty(k) && typeof body[k] !== 'undefined' && body[k] !== null && body[k] !== '') {
+            options.query[k] = body[k];
+        }
+    }
+
+    let endpoints_;
+
+    if (typeof institucion !== "undefined" && typeof institucion === "object"){
+        const { nombre, supplier_id } = institucion;
+        options.query.institucionDependencia = nombre;
+        endpoints_ = endpoints.filter(e => e.supplier_id === supplier_id );
+    } else if (typeof nivel_gobierno !== 'undefined'&& nivel_gobierno !== null && nivel_gobierno !== ''){
+        endpoints_ = endpoints.filter(e => e.levels.includes(nivel_gobierno));
+    } else {
+        endpoints_ = endpoints;
+    }
+
+    let queries = endpoints_.map(endpoint => {
+        let options_ = JSON.parse(JSON.stringify(options));
+        return fetchData(endpoint, options_).catch(error => {
+            console.log(error);
+            return {
+                supplier_id: endpoint.supplier_id,
+                supplier_name: endpoint.supplier_name,
+                levels: endpoint.levels,
+                error: "Algo salió mal.",
+                totalRows: "No disponible"
+            }
+        });
+    });
+
+    Promise.all(queries).then( responses => {
+        //console.log(data);
+        let summary = responses.map (data => {
+            if (typeof data.error !== 'undefined'){
+                return data;
+            } else {
+                return {
+                    supplier_id: data.supplier_id,
+                    supplier_name: data.supplier_name,
+                    levels: data.levels,
+                    totalRows: data.pagination.totalRows
+                }
+            }
+        });
+
+        res.json(summary);
+    }).catch(error => {
+        console.log(error);
+        res.status(500).json({
+            error: "Algo salio mal..."
+        });
+    });
+
 });
 
 router.post('/search', (req, res) => {
-
     res.json({});
 });
 
