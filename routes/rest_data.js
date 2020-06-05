4dc015f4eb9b3c7495892caa0226414fc0906e71
Benito Perdomo <benito.perdomo.v@gmail.com>
2020-06-04 19:13:20 -0500

    set mensajes de error

--- routes/rest_data.js
+++ routes/rest_data.js
@@ -1,124 +1,145 @@
-const axios = require("axios");
-const qs = require("qs");
+const axios = require('axios');
+const qs = require('qs');
 
-const log4js = require("log4js");
-var logger = log4js.getLogger("rest_data.js");
-logger.level = "all";
+const log4js = require('log4js');
+var logger = log4js.getLogger('rest_data.js');
+logger.level = 'all';
 
 const fetchEntities = (endpoint) => {
-  return getToken(endpoint).then((response) => {
-    if (typeof response.error !== "undefined") {
-      return { error: true };
-    }
+	return getToken(endpoint).then((response) => {
+		if (typeof response.error !== 'undefined') {
+			return { error: true };
+		}
 
-    const { data } = response;
-    const { access_token } = data;
+		const { data } = response;
+		const { access_token } = data;
 
-    const opts = {
-      url: endpoint.entities_url,
-      method: "GET",
-      params: {
-        access_token: access_token,
-      },
-      json: true,
-    };
+		const opts = {
+			url: endpoint.entities_url,
+			method: 'GET',
+			params: {
+				access_token: access_token
+			},
+			json: true
+		};
 
-    return axios(opts).then((response) => {
-      const entities = response.data;
-      return entities.map((e) => {
-        e.supplier_id = endpoint.supplier_id;
-        return e;
-      });
-    });
-  });
+		return axios(opts).then((response) => {
+			const entities = response.data;
+			return entities.map((e) => {
+				e.supplier_id = endpoint.supplier_id;
+				return e;
+			});
+		});
+	});
 };
 
 const getToken = (endpoint) => {
-  let data = {
-    grant_type: "password",
-    username: endpoint.username,
-    password: endpoint.password,
-  };
-  if (endpoint.scope !== "") {
-    data.scope = endpoint.scope;
-  }
+	let data = {
+		grant_type: 'password',
+		username: endpoint.username,
+		password: endpoint.password
+	};
+	if (endpoint.scope !== '') {
+		data.scope = endpoint.scope;
+	}
 
-  const opts = {
-    url: endpoint.token_url,
-    method: "post",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Authorization:
-        "Basic " +
-        Buffer.from(`${endpoint.client_id}:${endpoint.client_secret}`).toString(
-          "base64"
-        ),
-    },
-    data: qs.stringify(data),
-    json: true,
-  };
+	const opts = {
+		url: endpoint.token_url,
+		method: 'post',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			Authorization: 'Basic ' + Buffer.from(`${endpoint.client_id}:${endpoint.client_secret}`).toString('base64')
+		},
+		data: qs.stringify(data),
+		json: true
+	};
 
-  return axios(opts).catch((e) => {
-    return { error: e.response };
-  });
+	return axios(opts);
 };
 
 const fetchData = (endpoint, options) => {
-  return getToken(endpoint).then((response) => {
-    if (typeof response.error !== "undefined") {
-      let { status, statusText, data } = response.error;
+	let { supplier_id } = endpoint;
+	return getToken(endpoint)
+		.then((response) => {
+			if (typeof response.error !== 'undefined') {
+				logger.info(supplier_id, 'paso por aqui...');
+				let { status, statusText, data } = response.error;
 
-      logger.error("status:" + status);
-      logger.error("statusText:" + statusText);
-      logger.error("data:" + data);
+				logger.error(supplier_id, 'status:' + status);
+				logger.error(supplier_id, 'statusText:' + statusText);
+				logger.error(supplier_id, 'data:' + data);
 
-      return { error: { status, statusText, data } };
-    }
+				return { error: { status, statusText, data } };
+			}
 
-    const { access_token } = response.data;
+			const { access_token } = response.data;
 
-    let opts = {
-      url: endpoint.url,
-      method: "post",
-      headers: {
-        Authorization: "Bearer " + access_token,
-      },
-      data: options,
-      json: true,
-    };
+			let opts = {
+				url: endpoint.url,
+				method: 'post',
+				headers: {
+					Authorization: 'Bearer ' + access_token
+				},
+				data: options,
+				json: true
+			};
 
-    return axios(opts)
-      .then((request) => {
-        let { data } = request;
+			return axios(opts)
+				.then((request) => {
+					let { data } = request;
 
-        data.supplier_name = endpoint.supplier_name;
-        data.supplier_id = endpoint.supplier_id;
-        data.levels = endpoint.levels;
-        data.endpoint_type = endpoint.type;
+					data.supplier_name = endpoint.supplier_name;
+					data.supplier_id = endpoint.supplier_id;
+					data.levels = endpoint.levels;
+					data.endpoint_type = endpoint.type;
 
-        if (typeof data.code !== "undefined") {
-          return {
-            supplier_name: endpoint.supplier_name,
-            supplier_id: endpoint.supplier_id,
-            levels: endpoint.levels,
-            endpoint_type: endpoint.type,
-            error: {
-              code: data.code,
-              message: data.message,
-            },
-          };
-        }
+					if (typeof data.code !== 'undefined') {
+						return {
+							supplier_name: endpoint.supplier_name,
+							supplier_id: endpoint.supplier_id,
+							levels: endpoint.levels,
+							endpoint_type: endpoint.type,
+							error: {
+								code: data.code,
+								message: data.message
+							}
+						};
+					}
 
-        return data;
-      })
-      .catch((e) => {
-        logger.error(e);
-        return { error: true };
-      });
-  });
+					return data;
+				})
+				.catch((e) => {
+					let error =
+						typeof e.response === 'undefined'
+							? { error: { status: e.errno, statusText: e.code } }
+							: {
+									error: {
+										status: e.response.status,
+										statusText: e.response.statusText,
+										data: e.response.data
+									}
+								};
+
+					logger.error(supplier_id, error);
+					delete error.error.data;
+					return error;
+				});
+		})
+		.catch((e) => {
+			let error =
+				typeof e.response === 'undefined'
+					? { error: { status: e.errno, statusText: e.code } }
+					: {
+							error: { status: e.response.status, statusText: e.response.statusText, data: e.response.data }
+						};
+
+			logger.error(supplier_id, error);
+			delete error.error.data;
+			return error;
+		});
 };
 
 module.exports = {
-  fetchData,
-  fetchEntities,
+	fetchData,
+	fetchEntities
 };
--- routes/v1.js
+++ routes/v1.js
@@ -1,277 +1,228 @@
-var express = require("express");
-const cors = require("cors");
-const qs = require("qs");
-const { fetchData } = require("./rest_data");
-const endpoints = require("../endpoints");
+var express = require('express');
+const cors = require('cors');
+const qs = require('qs');
+const { fetchData } = require('./rest_data');
+const endpoints = require('../endpoints');
 
-const log4js = require("log4js");
-var logger = log4js.getLogger("v1.js");
-logger.level = "all";
+const log4js = require('log4js');
+var logger = log4js.getLogger('v1.js');
+logger.level = 'all';
 
 var router = express.Router();
 router.use(cors());
 
-router.get("/", (req, res) => {
-  logger.info("It's live!!!");
+router.get('/', (req, res) => {
+	logger.info("It's live!!!");
 
-  res.json({
-    title: "API del Sistema 1",
-    version: "1.0",
-  });
+	res.json({
+		title: 'API del Sistema 1',
+		version: '1.0'
+	});
 });
 
-router.get("/v1/providers", (req, res) => {
-  res.json(
-    endpoints.map((p) => {
-      return {
-        supplier_id: p.supplier_id,
-        supplier_name: p.supplier_name,
-        levels: p.levels,
-      };
-    })
-  );
+router.get('/v1/providers', (req, res) => {
+	res.json(
+		endpoints.map((p) => {
+			return {
+				supplier_id: p.supplier_id,
+				supplier_name: p.supplier_name,
+				levels: p.levels
+			};
+		})
+	);
 });
 
-router.post("/v1/summary", (req, res) => {
-  const { body } = req;
-  const { nivel_gobierno, institucion } = body;
+router.post('/v1/summary', (req, res) => {
+	const { body } = req;
+	const { nivel_gobierno, institucion } = body;
 
-  let options = {
-    page: 1,
-    pageSize: 1,
-    query: {},
-  };
+	let options = {
+		page: 1,
+		pageSize: 1,
+		query: {}
+	};
 
-  const params = ["nombres", "primerApellido", "segundoApellido"];
+	const params = [ 'nombres', 'primerApellido', 'segundoApellido' ];
 
-  for (let k of params) {
-    if (
-      body.hasOwnProperty(k) &&
-      typeof body[k] !== "undefined" &&
-      body[k] !== null &&
-      body[k] !== ""
-    ) {
-      options.query[k] = body[k];
-    }
-  }
+	for (let k of params) {
+		if (body.hasOwnProperty(k) && typeof body[k] !== 'undefined' && body[k] !== null && body[k] !== '') {
+			options.query[k] = body[k];
+		}
+	}
 
-  let endpoints_;
+	let endpoints_;
 
-  if (typeof institucion !== "undefined" && typeof institucion === "object") {
-    const { nombre, supplier_id } = institucion;
-    options.query.institucionDependencia = nombre;
-    endpoints_ = endpoints.filter((e) => e.supplier_id === supplier_id);
-  } else if (
-    typeof nivel_gobierno !== "undefined" &&
-    nivel_gobierno !== null &&
-    nivel_gobierno !== ""
-  ) {
-    endpoints_ = endpoints.filter((e) => e.levels.includes(nivel_gobierno));
-  } else {
-    endpoints_ = endpoints;
-  }
+	if (typeof institucion !== 'undefined' && typeof institucion === 'object') {
+		const { nombre, supplier_id } = institucion;
+		options.query.institucionDependencia = nombre;
+		endpoints_ = endpoints.filter((e) => e.supplier_id === supplier_id);
+	} else if (typeof nivel_gobierno !== 'undefined' && nivel_gobierno !== null && nivel_gobierno !== '') {
+		endpoints_ = endpoints.filter((e) => e.levels.includes(nivel_gobierno));
+	} else {
+		endpoints_ = endpoints;
+	}
 
-  let queries = endpoints_.map((endpoint) => {
-    let options_ = JSON.parse(JSON.stringify(options));
-    return fetchData(endpoint, options_).catch((error) => {
-      logger.error(error);
-      return {
-        supplier_id: endpoint.supplier_id,
-        supplier_name: endpoint.supplier_name,
-        levels: endpoint.levels,
-        error: "Algo salió mal.",
-        totalRows: "No disponible",
-      };
-    });
-  });
+	let queries = endpoints_.map((endpoint) => {
+		let options_ = JSON.parse(JSON.stringify(options));
+		return fetchData(endpoint, options_).catch((error) => {
+			logger.error(error);
+			return {
+				supplier_id: endpoint.supplier_id,
+				supplier_name: endpoint.supplier_name,
+				levels: endpoint.levels,
+				error: 'Algo salió mal.',
+				totalRows: 'No disponible'
+			};
+		});
+	});
 
-  Promise.all(queries)
-    .then((responses) => {
-      let summary = responses.map((data) => {
-        if (typeof data.error !== "undefined") {
-          return data;
-        } else {
-          return {
-            supplier_id: data.supplier_id,
-            supplier_name: data.supplier_name,
-            levels: data.levels,
-            pagination: data.pagination,
-            results: data.results,
-          };
-        }
-      });
-      res.json(summary);
-    })
-    .catch((error) => {
-      logger.error(error);
-      res.status(500).json({
-        error: "Algo salio mal...",
-      });
-    });
+	Promise.all(queries)
+		.then((responses) => {
+			let summary = responses.map((data) => {
+				if (typeof data.error !== 'undefined') {
+					return data;
+				} else {
+					return {
+						supplier_id: data.supplier_id,
+						supplier_name: data.supplier_name,
+						levels: data.levels,
+						pagination: data.pagination,
+						results: data.results
+					};
+				}
+			});
+			res.json(summary);
+		})
+		.catch((error) => {
+			logger.error(error);
+			res.status(500).json({
+				error: 'Algo salio mal...'
+			});
+		});
 });
 
 function createQuery(req, res, next) {
-  let { query } = req.body;
-  let _query = {};
+	let { query, supplier_id } = req.body;
+	let _query = {};
 
-  if (
-    typeof query === "undefined" ||
-    query === null ||
-    Object.entries(query).length === 0
-  ) {
-    next();
-    return;
-  }
+	if (typeof query === 'undefined' || query === null || Object.entries(query).length === 0) {
+		next();
+		return;
+	}
 
-  const params = [
-    "id",
-    "nombres",
-    "primerApellido",
-    "segundoApellido",
-    "escolaridadNivel",
-    "formaAdquisicion",
-  ];
+	const params = [ 'id', 'nombres', 'primerApellido', 'segundoApellido', 'escolaridadNivel', 'formaAdquisicion' ];
 
-  const empleo = [
-    "nombreEntePublico",
-    "entidadFederativa",
-    "municipioAlcaldia",
-    "empleoCargoComision",
-    "nivelOrdenGobierno",
-    "nivelEmpleoCargoComision",
-  ];
+	const empleo = [
+		'nombreEntePublico',
+		'entidadFederativa',
+		'municipioAlcaldia',
+		'empleoCargoComision',
+		'nivelOrdenGobierno',
+		'nivelEmpleoCargoComision'
+	];
 
-  const rangos = [
-    "superficieConstruccion",
-    "superficieTerreno",
-    "valorAdquisicion",
-    "totalIngresosNetos",
-  ];
+	const rangos = [ 'superficieConstruccion', 'superficieTerreno', 'valorAdquisicion', 'totalIngresosNetos' ];
 
-  logger.info("search query: ", qs.stringify(query));
+	logger.info(supplier_id, '|search query|', query);
 
-  params.forEach((p) => {
-    if (
-      typeof query[p] !== "undefined" &&
-      query[p] !== null &&
-      query[p] !== "0" &&
-      query[p].length > 0
-    ) {
-      if (p === "formaAdquisicion") {
-        _query["bienesInmuebles"] = {
-          ..._query["bienesInmuebles"],
-          formaAdquisicion: query[p],
-        };
-      } else {
-        _query[p] = query[p];
-      }
-    }
-  });
+	params.forEach((p) => {
+		if (typeof query[p] !== 'undefined' && query[p] !== null && query[p] !== '0' && query[p].length > 0) {
+			if (p === 'formaAdquisicion') {
+				_query['bienesInmuebles'] = {
+					..._query['bienesInmuebles'],
+					formaAdquisicion: query[p]
+				};
+			} else {
+				_query[p] = query[p];
+			}
+		}
+	});
 
-  empleo.forEach((p) => {
-    if (
-      typeof query[p] !== "undefined" &&
-      query[p] !== null &&
-      query[p] !== "0" &&
-      query[p].length > 0
-    ) {
-      _query["datosEmpleoCargoComision"] = {
-        ..._query["datosEmpleoCargoComision"],
-        [p]: query[p],
-      };
-    }
-  });
+	empleo.forEach((p) => {
+		if (typeof query[p] !== 'undefined' && query[p] !== null && query[p] !== '0' && query[p].length > 0) {
+			_query['datosEmpleoCargoComision'] = {
+				..._query['datosEmpleoCargoComision'],
+				[p]: query[p]
+			};
+		}
+	});
 
-  rangos.forEach((r) => {
-    let temp = null;
-    let min = query[r + "Min"];
-    let max = query[r + "Max"];
+	rangos.forEach((r) => {
+		let temp = null;
+		let min = query[r + 'Min'];
+		let max = query[r + 'Max'];
 
-    if (
-      typeof min !== "undefined" &&
-      min !== null &&
-      !isNaN(min) &&
-      min !== ""
-    ) {
-      temp = { ...temp, min: Number(min) };
-    }
+		if (typeof min !== 'undefined' && min !== null && !isNaN(min) && min !== '') {
+			temp = { ...temp, min: Number(min) };
+		}
 
-    if (
-      typeof max !== "undefined" &&
-      max !== null &&
-      !isNaN(max) &&
-      min !== ""
-    ) {
-      temp = { ...temp, max: Number(max) };
-    }
+		if (typeof max !== 'undefined' && max !== null && !isNaN(max) && min !== '') {
+			temp = { ...temp, max: Number(max) };
+		}
 
-    if (temp !== null) {
-      if (r === "totalIngresosNetos") {
-        _query[r] = temp;
-      } else {
-        _query["bienesInmuebles"] = { ..._query["bienesInmuebles"], [r]: temp };
-      }
-    }
-  });
+		if (temp !== null) {
+			if (r === 'totalIngresosNetos') {
+				_query[r] = temp;
+			} else {
+				_query['bienesInmuebles'] = { ..._query['bienesInmuebles'], [r]: temp };
+			}
+		}
+	});
 
-  req.body.query = _query;
+	req.body.query = _query;
 
-  next();
+	next();
 }
 
 function crateOrder(req, res, next) {
-  next();
+	next();
 }
 
-router.post("/v1/search", createQuery, crateOrder, (req, res) => {
-  const { query } = req.body;
+router.post('/v1/search', createQuery, crateOrder, (req, res) => {
+	let { query, supplier_id, institucion, page, pageSize } = req.body;
 
-  const { body } = req;
-  const { supplier_id, institucion } = body;
-  let { page, pageSize } = body;
+	logger.info(supplier_id, '|query|', query);
 
-  logger.info("query: ", qs.stringify(query));
+	if (typeof page === 'undefined' || page === null || isNaN(page)) {
+		page = 1;
+	}
 
-  if (typeof page === "undefined" || page === null || isNaN(page)) {
-    page = 1;
-  }
+	if (typeof pageSize === 'undefined' || pageSize === null || isNaN(pageSize)) {
+		pageSize = 10;
+	}
 
-  if (typeof pageSize === "undefined" || pageSize === null || isNaN(pageSize)) {
-    pageSize = 10;
-  }
+	if (typeof supplier_id === 'undefined') {
+		res.status(500).json({
+			error: 'Debe proporcionar un proveedor de información'
+		});
+		return;
+	}
 
-  if (typeof supplier_id === "undefined") {
-    res.status(500).json({
-      error: "Debe proporcionar un proveedor de información",
-    });
-    return;
-  }
+	let endpoint = endpoints.find((d) => d.supplier_id === supplier_id);
 
-  let endpoint = endpoints.find((d) => d.supplier_id === supplier_id);
+	if (typeof endpoint === 'undefined') {
+		res.status(500).json({
+			error: 'Proveedor de información no disponible'
+		});
+		return;
+	}
 
-  if (typeof endpoint === "undefined") {
-    res.status(500).json({
-      error: "Proveedor de información no disponible",
-    });
-    return;
-  }
+	let options = {
+		page,
+		pageSize,
+		query: query
+	};
 
-  let options = {
-    page,
-    pageSize,
-    query: query,
-  };
+	logger.info(supplier_id, '|query final|', options);
 
-  logger.info("query final: ", qs.stringify(options));
-
-  fetchData(endpoint, options)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      logger.error(error);
-      res.status(500).json({ error: "Algo salió mal..." });
-    });
+	fetchData(endpoint, options)
+		.then((data) => {
+			res.json(data);
+		})
+		.catch((error) => {
+			logger.error(error);
+			res.status(500).json({ error: { status, statusText, data } });
+		});
 });
 
 module.exports = router;
